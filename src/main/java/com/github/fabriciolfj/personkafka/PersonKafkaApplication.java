a531bdc5744e976198cbeaeb31e560c5f7dfd560
fabricio jacob <fabricio.jacob@outlook.com>
2023-01-27 22:13:55 -0300

    add

---
+++ src/main/java/com/github/fabriciolfj/personkafka/PersonKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.personkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fabriciolfj/personkafka/PersonKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.personkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
